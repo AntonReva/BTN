f316bf33b257774260f83fbaf9825b9c05fded1e
Anton Reva <boss.kvanta@gmail.com>
2022-09-22 19:36:38 +0300

    for Niko

--- js.js
+++ js.js
@@ -2,5 +2,5 @@ const btn = document.querySelector("#btn");
 btn.addEventListener("click", btnHandler);
 
 function btnHandler() {
-  return window.open("https://www.instagram.com/ntskkk/");
+  return window.open("https://www.instagram.com/__f.o.r.s.e__/");
 }
