5d7d52d6e258de8cc511bfb27cdbc66f2925e6e6
Anton Reva <boss.kvanta@gmail.com>
2022-09-22 18:57:17 +0300

    new files

---
+++ js.js
@@ -0,0 +1,6 @@
+const btn = document.querySelector("#btn");
+btn.addEventListener("click", btnHandler);
+
+function btnHandler() {
+  return window.open("https://www.instagram.com/__f.o.r.s.e__/");
+}
